777cd242b6dcfda516db5b2f2aa09cc12684d239
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-07 07:42:46 +0300

    add: modal window

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <div id="app">
-<h1>Hallo</h1>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <button class="btn" @click="modalFirst = !modalFirst">Show first modal</button>
+          <!-- first modal -->
+          <modals
+              title="First modal"
+              v-show="modalFirst"
+              @close="modalFirst = !modalFirst">
+            <!-- body -->
+            <div slot="body">
+              <p> Text Text Text Text Text Text Text </p>
+              <button class="btn" @click="modalFirst = !modalFirst">Well Done!</button>
+            </div>
+          </modals>
+
+        </div>
+      </section>
+
+
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import modals from '@/components/Modal.vue'
 export default {
-  name: 'App',
-  components: {
+  components: { modals },
+  data () {
+    return {
+      modalFirst: false
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.btn {
+  padding: 15px;
+  background-color: coral;
+  border-radius: 8px;
+  cursor: pointer;
+  font-family: inherit;
 }
+
 </style>
