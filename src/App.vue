8f424b1e8df3813285fb427cf0b65988cf35ffa0
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-07 08:56:50 +0300

    add: modal form

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,35 @@
       <section>
         <div class="container">
           <button class="btn" @click="modalFirst = !modalFirst">Show first modal</button>
+
           <!-- first modal -->
           <modals
               title="First modal"
               v-show="modalFirst"
-              @close="modalFirst = !modalFirst">
+              @close="modalFirst = false">
             <!-- body -->
             <div slot="body">
               <p> That slot works </p>
-              <button class="btn" @click="modalFirst = !modalFirst">Close modal!</button>
+              <button class="btn" @click="modalFirst = false">Close modal!</button>
+            </div>
+          </modals>
+
+          <!-- second modal -->
+          <button class="btn" @click="modalSecond.show = !modalSecond.show">Show second modal</button>
+          <modals
+              title="Modal with form"
+              v-show="modalSecond.show"
+              @close="modalSecond.show = false">
+            <!-- body -->
+            <div slot="body">
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input type="text" required v-model="modalSecond.name">
+                <label>E-mail:</label>
+                <input type="email" required v-model="modalSecond.email">
+                <button class="btn">Submit</button>
+              </form>
+
             </div>
           </modals>
         </div>
@@ -23,14 +43,32 @@
 
 <script>
 import modals from '@/components/Modal.vue'
+
 export default {
-  components: { modals },
-  data () {
+  components: {modals},
+  data() {
     return {
-      modalFirst: false
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      })
+      this.modalSecond.name = '';
+          this.modalSecond.email = '';
+          this.modalSecond.show = false;
     }
   }
 }
+
 </script>
 <style lang="scss" scoped>
 .btn {
@@ -39,6 +77,19 @@ export default {
   border-radius: 8px;
   cursor: pointer;
   font-family: inherit;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  input {
+    height: auto;
+    border-radius: 6px;
+    padding: 10px;
+    margin: 10px;
+    min-width: 400px;
+  }
 }
 
 </style>
